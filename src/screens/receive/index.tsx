bd3672f110fb004944c37114487810bc1b623fb3
Byron H <bh@axul.net>
2023-04-24 01:47:35 -0500

    Add qr code to receive screen.

--- src/screens/receive/index.tsx
+++ src/screens/receive/index.tsx
@@ -1,14 +1,18 @@
 import { useNavigation } from '@react-navigation/native';
+import { useAtomValue } from 'jotai';
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Appbar, Text } from 'react-native-paper';
+import QRCode from 'react-native-qrcode-svg';
 
 import { RootStackParamList } from 'src/constants/RootStackParamList';
+import { profileAtom } from 'src/state/user';
 
 type Props = {};
 
 const ReceiveScreen = (props: Props) => {
   const navigation = useNavigation<RootStackParamList>();
+  const profile = useAtomValue(profileAtom);
 
   return (
     <View>
@@ -17,8 +21,22 @@ const ReceiveScreen = (props: Props) => {
         <Appbar.Content title="Receive" />
       </Appbar.Header>
       <Text>Detail</Text>
+      {profile?.lightning_address && (
+        <View style={styles.container}>
+          <QRCode value={`lightning:${profile?.lightning_address}`} size={250} />
+        </View>
+      )}
     </View>
   );
 };
 
 export default ReceiveScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+});
