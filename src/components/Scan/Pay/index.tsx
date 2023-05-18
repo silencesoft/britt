9f4c90c7da77b03bf5b975daa597b56706978b06
Byron H <bh@axul.net>
2023-05-18 13:05:06 -0500

    Update pay screen.

--- src/components/Scan/Pay/index.tsx
+++ src/components/Scan/Pay/index.tsx
@@ -1,9 +1,11 @@
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import { useAtomValue } from 'jotai';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { ActivityIndicator, Text } from 'react-native-paper';
+import { ActivityIndicator, Appbar, Text } from 'react-native-paper';
 
+import { RootStackParamList } from 'src/constants/RootStackParamList';
 import { doPayment } from 'src/services/doPayment';
 import { userAtom } from 'src/state/user';
 
@@ -13,8 +15,10 @@ type Props = {
 
 const Pay = ({ invoice }: Props) => {
   const [isValid, setIsValid] = useState(invoice.startsWith('lnbc'));
+  // const isEmail = /\b[a-z0-9-_.]+@[a-z0-9-_.]+(\.[a-z0-9]+)+/i.test(invoice);
   const [success, setSuccess] = useState(false);
   const user = useAtomValue(userAtom);
+  const navigation = useNavigation<RootStackParamList>();
 
   useEffect(() => {
     const tryPayment = async () => {
@@ -35,21 +39,34 @@ const Pay = ({ invoice }: Props) => {
 
   if (!isValid) {
     return (
-      <View style={styles.container}>
-        <Text>Invalid invoice.</Text>
+      <View style={{ flex: 1, width: '100%' }}>
+        <Appbar.Header>
+          <Appbar.BackAction onPress={() => navigation.navigate('Scan')} />
+          <Appbar.Content title="Receive" />
+        </Appbar.Header>
+        <View style={styles.container}>
+          <Text>Invalid invoice.</Text>
+          <Text>Only bolt11 invoices supported.</Text>
+        </View>
       </View>
     );
   }
 
   return (
-    <View style={styles.container}>
-      {success && (
-        <>
-          <Feather name="check-circle" size={48} />
-          <Text style={{ marginTop: 40 }}>Payment done.</Text>
-        </>
-      )}
-      {!success && <ActivityIndicator />}
+    <View style={{ flex: 1, width: '100%' }}>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => navigation.navigate('Scan')} />
+        <Appbar.Content title="Receive" />
+      </Appbar.Header>
+      <View style={styles.container}>
+        {success && (
+          <>
+            <Feather name="check-circle" size={48} />
+            <Text style={{ marginTop: 40 }}>Payment done.</Text>
+          </>
+        )}
+        {!success && <ActivityIndicator />}
+      </View>
     </View>
   );
 };
